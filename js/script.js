251e3e5882c7e51c8f90e8ee2286f608b60bd43a
SaadKebdani <saadkebdani2002@gmail.com>
2024-01-23 16:11:54 +0100

    projet QCM

---
+++ js/script.js
@@ -0,0 +1,65 @@
+let question = [ 
+    {
+        question: "how many ballon d'or does CR7 have?",
+        Reponse1:[5, true],
+        Reponse2:[6, false],
+        Reponse3:[7, false],
+        Reponse4:[8, false],
+
+    },
+    {
+     question: "how many ballon d'or does CR7 have?",
+        Reponse1:[5, false],
+        Reponse2:[6, false],
+        Reponse3:[7, false],
+        Reponse4:[8, true],
+
+   },
+        
+   {
+         question: "how many ballon d'or does CR7 have?",
+        Reponse1:[5, false],
+        Reponse2:[6, false],
+        Reponse3:[7, false],
+        Reponse4:[8, true],
+    
+  },
+
+ {
+     question: "how many ballon d'or does CR7 have?",
+        Reponse1:[5, false],
+        Reponse2:[6, false],
+        Reponse3:[7, false],
+        Reponse4:[8, true],
+ 
+ },
+ 
+   {
+        question: "how many ballon d'or does CR7 have?",
+        Reponse1:[5, false],
+        Reponse2:[6, false],
+        Reponse3:[7, false],
+        Reponse4:[8, true],
+
+            
+   },
+    
+    ]
+    
+    let titre = document.getElementById("question")
+    titre.innerHTML = question[1].question
+
+//    let reponse = document.querySelectorAll("label")
+//    reponse[0].innerHTML= "saad2"
+//     reponse[1].innerHTML= "saad2"
+
+
+
+
+
+
+
+
+
+
+
